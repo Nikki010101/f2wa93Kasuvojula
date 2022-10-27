faef6284c2a67b29e050f9419662ff6271b4a971
Nikki010101 <111778589+Nikki010101@users.noreply.github.com>
2022-10-26 21:05:29 -0500

    Update app.js

--- app.js
+++ app.js
@@ -37,7 +37,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-const server = app.listen(process.env.PORT || 3000, () => {
-  console.log(`${server.address().port}`);
-});
+
 module.exports = app;
